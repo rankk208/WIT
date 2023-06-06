893c358039fa2743913366c579d8bd2909dbb8e4
novra <nunohida@gmail.com>
2023-06-06 08:14:09 +0700

    Add files via upload

--- ecomm/toko/urls.py
+++ ecomm/toko/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'toko'
 
 urlpatterns = [
      path('', views.HomeListView.as_view(), name='home-produk-list'),
      path('product/<slug>/', views.ProductDetailView.as_view(), name='produk-detail'),
      path('checkout/', views.CheckoutView.as_view(), name='checkout'),
+     ##path('contact/', views.ContactView.as_view(), name='contact'),
+     path('contact/', views.ContactView.as_view(), name='contact'),
+     path('success/', views.SuccessView.as_view(), name='success'),
      path('add-to-cart/<slug>/', views.add_to_cart, name='add-to-cart'),
      path('remove_from_cart/<slug>/', views.remove_from_cart, name='remove-from-cart'),
      path('order-summary/', views.OrderSummaryView.as_view(), name='order-summary'),
@@ -14,3 +18,5 @@ urlpatterns = [
      path('paypal-return/', views.paypal_return, name='paypal-return'),
      path('paypal-cancel/', views.paypal_cancel, name='paypal-cancel'),
 ]
+
+
